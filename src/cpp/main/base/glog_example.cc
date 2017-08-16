ab811a5a3f1d64a0ffd09772baff94882f9a2645
Zhongming Qu <qzmfranklin@gmail.com>
2017-08-16 01:06:16 -0400

    update glog_example

--- src/cpp/main/base/glog_example.cc
+++ src/cpp/main/base/glog_example.cc
@@ -1,21 +1,20 @@
-#include <glog/logging.h>
 #include <gflags/gflags.h>
+#include <glog/logging.h>
 
 DEFINE_bool(big_menu, true, "Include 'advanced' options in the menu listing");
 
 int main(int argc, char *argv[])
 {
-	gflags::SetVersionString("1.0.0.0");
-	gflags::SetUsageMessage("Usage : ./demo ");
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
-	google::InitGoogleLogging(argv[0]);
+    google::SetVersionString("1.0.0.0");
+    google::SetUsageMessage("Usage : ./demo ");
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    google::InitGoogleLogging(argv[0]);
 
-    //google::InstallFailureSignalHandler();
-    //google::InstallFailureFunction(&YourFailureFunction);
+    // google::InstallFailureSignalHandler();
+    // google::InstallFailureFunction(&YourFailureFunction);
 
-	LOG(INFO) << "Found " << 123213L << " cookies" ;
+    LOG(INFO) << "Found " << 123213L << " cookies";
     LOG(WARNING) << "big_menu= " << FLAGS_big_menu;
-	LOG(ERROR) << "FOO";
-    gflags::ShutDownCommandLineFlags();
-
+    LOG(ERROR) << "FOO";
+    google::ShutDownCommandLineFlags();
 }
