dba9e9af74f720e6f6bc7e83a6f5ac08492a4fe0
Zhongming Qu <qzmfranklin@gmail.com>
2017-11-21 10:47:40 -0800

    Improve src/cpp/main/base:eglog_example

--- src/cpp/main/base/glog_example.cc
+++ src/cpp/main/base/glog_example.cc
@@ -3,18 +3,20 @@
 
 DEFINE_bool(big_menu, true, "Include 'advanced' options in the menu listing");
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     google::SetVersionString("1.0.0.0");
     google::SetUsageMessage("Usage : ./demo ");
     google::ParseCommandLineFlags(&argc, &argv, true);
     google::InitGoogleLogging(argv[0]);
-
-    // google::InstallFailureSignalHandler();
+    google::InstallFailureSignalHandler();
     // google::InstallFailureFunction(&YourFailureFunction);
-
-    LOG(INFO) << "Found " << 123213L << " cookies";
-    LOG(WARNING) << "big_menu= " << FLAGS_big_menu;
-    LOG(ERROR) << "FOO";
     google::ShutDownCommandLineFlags();
+
+    LOG(WARNING) << "Found " << 123213L << " cookies";
+    LOG(INFO) << "FOO";
+    LOG(ERROR) << "big_menu= " << FLAGS_big_menu;
+
+    LOG(ERROR) << "About to hit a check failure and abort ...";
+    CHECK_EQ(1, 0);
 }
