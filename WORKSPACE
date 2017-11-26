e7f6e36e7ba892ae1775e19a19c25e81e8b4a30b
Zhongming Qu <qzmfranklin@gmail.com>
2017-11-25 17:30:34 -0800

    Add python pex support and refactor WORKSPACE.

--- WORKSPACE
+++ WORKSPACE
@@ -1,5 +1,7 @@
 workspace(name = 'torrent')
 
-load('//bazel:workspace.bzl', 'torrent_repositories')
-
+load('//bazel:workspace.bzl', torrent_repositories = 'repositories')
 torrent_repositories()
+
+load('@io_bazel_rules_pex//pex:pex_rules.bzl', 'pex_repositories')
+pex_repositories()
---
+++ src/python/demo_pex/foo_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""One-line summary"""
+
+import src.python.demo_pex.foo as foo
+
+import flask
+import yaml
+
+
+def test_foo():
+    """Main"""
+    foo.main()
+
+
+def test_yaml():
+    yaml.dump({"herp": ["derp", "derp", "derp"]})
+
+
+def test_flask():
+    app = flask.Flask("foo")
+
+
+def test_foo2():
+    assert True
